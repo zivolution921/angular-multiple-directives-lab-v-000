acc5078c8379736202828554551a7835051380e3
Todd Motto <todd@toddmotto.com>
2016-02-16 23:16:25 +0000

    chore: added base directory structure

---
+++ js/app/controllers/ContactController.js
@@ -0,0 +1,20 @@
+function ContactController() {
+    this.contacts = [{
+        name: 'Bob',
+        phone: '0123458690'
+    },{
+        name: 'Tim',
+        phone: '3934203242'
+    },{
+        name: 'Ross',
+        phone: '0684059433'
+    }];
+
+    this.removeContact = function (index) {
+        this.contacts.splice(index, 1);
+    }
+}
+
+angular
+    .module('app')
+    .controller('ContactController', ContactController);
---
+++ spec/Index.spec.js
@@ -0,0 +1,6 @@
+var path = require('path');
+
+describe('Contacts', function() {
+	browser.get('http://localhost:8080');
+
+});
